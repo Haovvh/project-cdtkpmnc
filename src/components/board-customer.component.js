7c2b0e4203f2a2f360bdf26d9f1f87651d210e87
Haovvh <haovvh@gmail.com>
2022-10-05 21:32:25 +0700

    Done API with Goong

--- src/components/board-customer.component.js
+++ src/components/board-customer.component.js
@@ -1,96 +1,99 @@
-import React, { useState, useEffect} from "react";
+import React, { useState} from "react";
 import GongMap from "../Goong/GoongMap";
-import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
-import TotalMoney from "./totalMoney.component";
+import ShowMoney from "./showMoney.component";
+import Form from "react-validation/build/form";
+import { API_KEY } from "../Goong/GoongKEY";
+import axios from "axios";
+import { json } from "react-router-dom";
 
+
+const required = value => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
 
 
 export default function BoardCustomer() {
-  const [distance, setDistance] = useState({
-    quangduong: "",
-    thoigian: ""
-  });
-  const [origins, setorigins] = useState("")
-  const [destinations, setdestinations] = useState("")
-
-  useEffect(() => {
-      async function fetchAPI() {       
-          try {
-              const resOrigin = await axios.get(`https://rsapi.goong.io/geocode?address=${prop.origins}&api_key=${API_KEY}`)
-              const jsonOrigin = await resOrigin.data.results[0].geometry.location.lat + ',' + resOrigin.data.results[0].geometry.location.lng
-              
-              if(jsonOrigin) {
-                  setorigins(jsonOrigin)
-              }
-              else {
-                  setorigins("")
-              }
-              const resdesti = await axios.get(`https://rsapi.goong.io/geocode?address=${prop.destinations}&api_key=${API_KEY}`)
-              const jsondesti = await resdesti.data.results[0].geometry.location.lat + ',' + resdesti.data.results[0].geometry.location.lng
-              
-              if (jsondesti) {
-                  setdestinations(jsondesti)
-              } else {
-                  setdestinations("")
-              }
-              console.log("123")
-              console.log(origins)
-              console.log(destinations)
-              console.log(321)
-              const res = await axios.get(`https://rsapi.goong.io/DistanceMatrix?origins=${origins}&destinations=${destinations}&vehicle=car&api_key=${API_KEY}`)
-              const json = await res.data.rows[0].elements[0]
-              console.log(json)
-              if (json) {
-                  setDistance({
-                      quangduong: json.distance.text,
-                      thoigian: json.duration.text
-                  })
-              }
-              else {
-                  setDistance({quangduong: "", thoigian: ""})
-              }
-              
-              
-          } catch (error) {
-              console.log(error)
-          }           
-      }
-      fetchAPI();
-  },[distance.quangduong])
-
+  const [distance, setDistance] = useState("")
+  const [duration, setDuration] = useState("")
+  const [placeFrom, setPlaceFrom] = useState("");
+  const [placeTo, setPlaceTo] = useState("");
+  const [show, setShow] = useState(true) 
+  const handlePlaceFrom = (event) => {
+    setPlaceFrom(event.target.value)
+  }
+  const handlePlaceTo = (event) => {
+    setPlaceTo(event.target.value)
+  }
+  const handleOnClick = async () => {
+    try {
+      const resOrigin = await axios.get(`https://rsapi.goong.io/geocode?address=${placeFrom}&api_key=${API_KEY}`)
+      const jsonOrigin = await resOrigin.data.results[0].geometry.location.lat + ',' + resOrigin.data.results[0].geometry.location.lng
+      
+      
+      const resdesti = await axios.get(`https://rsapi.goong.io/geocode?address=${placeTo}&api_key=${API_KEY}`)
+      const jsondesti = await resdesti.data.results[0].geometry.location.lat + ',' + resdesti.data.results[0].geometry.location.lng
+      
+      
+      const res = await axios.get(`https://rsapi.goong.io/DistanceMatrix?origins=${jsonOrigin}&destinations=${jsondesti}&vehicle=car&api_key=${API_KEY}`)
+      const json = await res.data.rows[0].elements[0]
+      setDistance("Quảng đường là: " + json.distance.text)
+      setDuration("Thời gian là: " + json.duration.text)
+      console.log(json.distance)
+      console.log(json.duration)                  
+      
+  } catch (error) {
+      console.log(error)
+  }
+  
+  }
+  
+//          {isCheck ? <TotalMoney origins="140 lê trọng tấn tân phú" destinations="66 nguyễn ngọc phương bình thạnh" /> : <h1></h1>}  
   return (
     <React.Fragment>
       <div className="col-md-12">
-        <div className="card card-container">
-          {isCheck ? <TotalMoney origins="140 lê trọng tấn tân phú" destinations="66 nguyễn ngọc phương bình thạnh" /> : <h1></h1>}         
-          
-          <Form >
+        <div className="card card-container">     
             <div>
-              <div className="form-group">
+              <h1>
+                {distance}
+              </h1>
+              <h1>
+                {duration}
+              </h1>
+            </div>
+            <div className="form-group">
                 <label htmlFor="username">Điểm đón:</label>
-                <Input
+                <input
                   placeholder="Điểm đón"
                   type="text"
                   className="form-control"
-                  name="placeFrom"
+                  value={placeFrom}
+                  onChange={(event) => {handlePlaceFrom(event)}}
                 />
               </div>
               <div className="form-group">
                 <label htmlFor="username">Điểm đến:</label>
-                <Input
+                <input
                   placeholder="Điểm đến"
                   type="text"
                   className="form-control"
-                  name="placeTo"
+                  value={placeTo}
+                  onChange={(event) => {handlePlaceTo(event)}}
                 />
               </div>
               <div className="form-group">
-                <button className="btn btn-primary btn-block" onClick={() => setIsCheck(!isCheck)}>Xác nhận</button>
+              <button className="btn btn-primary btn-block" onClick={() => { handleOnClick()
+                }}>
+                    {show ? 'Hide' : 'Show'}</button>
               </div>
-            </div>
-          </Form>
-
+            
+              
         </div>
       </div>
       <div>
--- src/components/totalMoney.component.js
+++ src/components/totalMoney.component.js
@@ -7,57 +7,6 @@ import { API_KEY } from "../Goong/GoongKEY";
 
 function TotalMoney(prop) {
     
-    const [distance, setDistance] = useState({
-        quangduong: "",
-        thoigian: ""
-    });
-    const [origins, setorigins] = useState("")
-    const [destinations, setdestinations] = useState("")
-    
-    useEffect(() => {
-        async function fetchAPI() {       
-            try {
-                const resOrigin = await axios.get(`https://rsapi.goong.io/geocode?address=${prop.origins}&api_key=${API_KEY}`)
-                const jsonOrigin = await resOrigin.data.results[0].geometry.location.lat + ',' + resOrigin.data.results[0].geometry.location.lng
-                
-                if(jsonOrigin) {
-                    setorigins(jsonOrigin)
-                }
-                else {
-                    setorigins("")
-                }
-                const resdesti = await axios.get(`https://rsapi.goong.io/geocode?address=${prop.destinations}&api_key=${API_KEY}`)
-                const jsondesti = await resdesti.data.results[0].geometry.location.lat + ',' + resdesti.data.results[0].geometry.location.lng
-                
-                if (jsondesti) {
-                    setdestinations(jsondesti)
-                } else {
-                    setdestinations("")
-                }
-                console.log("123")
-                console.log(origins)
-                console.log(destinations)
-                console.log(321)
-                const res = await axios.get(`https://rsapi.goong.io/DistanceMatrix?origins=${origins}&destinations=${destinations}&vehicle=car&api_key=${API_KEY}`)
-                const json = await res.data.rows[0].elements[0]
-                console.log(json)
-                if (json) {
-                    setDistance({
-                        quangduong: json.distance.text,
-                        thoigian: json.duration.text
-                    })
-                }
-                else {
-                    setDistance({quangduong: "", thoigian: ""})
-                }
-                
-                
-            } catch (error) {
-                console.log(error)
-            }           
-        }
-        fetchAPI();
-    },[distance.quangduong])
     
     
     if(prop.origins != "" || prop.destinations != "") {
